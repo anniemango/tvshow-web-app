08a1564a4d24ab1be6f049f913bb238c1f748bbd
Annie Yao <annieyao.mango@gmail.com>
2023-07-27 12:01:32 -0700

    adds app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { TvShowResultComponent } from './tv-show-result/tv-show-result.component';
-import { HttpClientModule } from '@angular/common/http';
+import { TvShowSearchComponent} from './tv-show-search/tv-show-search.component';
 
 @NgModule({
-  declarations: [
+    declarations: [
     AppComponent,
-    TvShowResultComponent
+    TvShowResultComponent,
+    TvShowSearchComponent
   ],
-  imports: [
+  imports:[
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
